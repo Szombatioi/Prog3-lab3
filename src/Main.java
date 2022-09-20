a35e6b6ae58d3e804a3b428a840579d95e950d04
Szombatioi <62300631+Szombatioi@users.noreply.github.com>
2022-09-20 13:26:47 +0200

    6. feladatig

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
 	protected static File wd = new File(System.getProperty("user.dir"));
+	private static Scanner input;
 	
 	protected static void pwd() {//most akkor 2 féleképp írjam le?
 		//System.out.print(System.getProperty("user.dir"));
@@ -15,7 +15,8 @@ public class Main {
 		} catch(IOException e) { System.out.println("Error."); }
 	}
 	
-	protected static void cd(String dir) {
+	protected static void cd(String[] cmd) {
+		String dir = cmd[1];
 		if(dir.equals("..")) {
 			wd = wd.getParentFile();
 		}
@@ -42,9 +43,74 @@ public class Main {
 		}
 	}
 	
+	protected static void rm(String[] cmd){
+		File f = new File(wd+"\\"+cmd[1]);
+		if(!f.delete()){
+			System.out.println("Error on deleting file: "+cmd[1]);
+		}
+	}
+	
+	protected static void mkdir(String[] cmd){
+//		cd(cmd);
+		File f = new File(wd.toString()+"\\"+cmd[1]);
+		if(f.exists()) System.out.println("Folder already exists!");
+		else f.mkdir();
+	}
+	
+	protected static void mv(String[] cmd){
+		File f = new File(wd.toString()+"\\"+cmd[1]);
+		if(!f.renameTo(new File(wd.toString()+"\\"+cmd[2]))) System.out.println("Error");
+	}
+	
+	protected static void cp(String[] cmd) throws IOException {
+		InputStream fis = new FileInputStream(cmd[1]);
+		Reader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(cmd[2])));
+		
+		String line = br.readLine();
+		while(line != null){
+			pw.println(line);
+			line = br.readLine();
+		}
+		
+		br.close();
+		pw.close();
+	}
+	
+	protected static void cat(String[] cmd) throws IOException{
+		InputStream fis = new FileInputStream(cmd[1]);
+		Reader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		String line = br.readLine();
+		while(line != null){
+			System.out.println(line);
+			line = br.readLine();
+		}
+		br.close();
+	}
+	
+	protected static void head(String[] cmd) throws IOException{
+		InputStream fis = new FileInputStream(cmd[1]);
+		Reader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		int lines = 10;
+		if(cmd.length>2) lines = Integer.parseInt(cmd[2]);
+		
+		String line = br.readLine();
+		while(line != null && lines>0){
+			System.out.println(line);
+			line = br.readLine();
+			lines--;
+		}
+		br.close();
+	}
 	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		input = new Scanner(System.in);
 		while(true) {
 			String[] cmd = input.nextLine().split(" ");
 			if(cmd[0].equals("exit")) {
@@ -57,7 +123,40 @@ public class Main {
 				ls(cmd);
 			}
 			else if(cmd[0].equals("cd")) {
-				cd(cmd[1]);
+				cd(cmd);
+			}
+			else if(cmd[0].equals("rm")){
+				rm(cmd);
+			}
+			else if(cmd[0].equals("mkdir")){
+				mkdir(cmd);
+			}
+			else if(cmd[0].equals("mv")){
+				mv(cmd);
+			}
+			else if(cmd[0].equals("cp")){
+				try{
+					cp(cmd);
+				}catch(IOException e){
+					System.out.println("IO Exception!");
+				}
+			}
+			else if(cmd[0].equals("cat")){
+				try{
+					cat(cmd);
+				}catch(IOException e){
+					System.out.println("IO Exception!");
+				}
+			}
+			else if(cmd[0].equals("head")){
+				try{
+					head(cmd);
+				}catch(IOException e){
+					System.out.println("IO Exception!");
+				}
+			}
+			else{
+				System.out.println("Error. No function named -="+cmd[0]+"=-");
 			}
 		}
 	}
